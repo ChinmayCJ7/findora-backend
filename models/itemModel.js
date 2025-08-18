e1542c3e656ce90ea175765d7b42585916de207c
Chinmay J <105866697+ChinmayCJ7@users.noreply.github.com>
2025-08-18 23:25:01 +0530

    updated models

---
+++ models/itemModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Item = mongoose.model("Item", itemSchema);
+
+export default Item;
